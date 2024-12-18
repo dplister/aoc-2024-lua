1046c62b256fa9043f38ddfd1544b14c723ad92c
dplister <dev@box>
2024-12-18 16:54:52 +1000

    Tests for list

--- list.lua
+++ list.lua
@@ -8,20 +8,19 @@ function list.arrays_equal(l1, l2)
     if #l1 ~= #l2 then 
         return false 
     end
-    local keys = {}
     local values = {}
+    -- increment first set
     for _, v in ipairs(l1) do
-        keys[#keys + 1] = v
-        values[v] = 1
-    end
-    for _, v in ipairs(l2) do
-        keys[#keys + 1] = v
         values[v] = (values[v] or 0) + 1
     end
-    for _, v in ipairs(keys) do
-        if values[v] ~= 2 then
-            return false
-        end
+    -- decrement second set
+    for _, v in ipairs(l2) do
+        if values[v] == nil or values[v] < 1 then return false end
+        values[v] = values[v] - 1
+    end
+    -- check first set again to see if all are zeroed
+    for _, v in ipairs(l1) do
+        if values[v] ~= 0 then return false end
     end
     return true
 end
--- test_list.lua
+++ test_list.lua
@@ -17,6 +17,22 @@ function test_arrays_equal_strings()
     luaunit.assertTrue(list.arrays_equal({ "a", "c" }, { "c", "a" }))
 end
 
+function test_arrays_multiple_same_equal()
+    luaunit.assertTrue(list.arrays_equal({ "a", "b", "a" }, { "b", "a", "a" }))
+    luaunit.assertTrue(list.arrays_equal({ "a", "a", "b" }, { "b", "a", "a" }))
+    luaunit.assertTrue(list.arrays_equal({ "a", "a", "b" }, { "a", "a", "b" }))
+    luaunit.assertTrue(list.arrays_equal({ }, { }))
+end
+
+function test_arrays_multiple_same_not_equal()
+    luaunit.assertFalse(list.arrays_equal({ "a", "b" }, { "a", "b", "a" }))
+    luaunit.assertFalse(list.arrays_equal({ "a", "b", "a" }, { "a", "b" }))
+    luaunit.assertFalse(list.arrays_equal({ "a", "b", "c" }, { "a", "b" }))
+    luaunit.assertFalse(list.arrays_equal({ "a", "b" }, { "a", "b", "c" }))
+    luaunit.assertFalse(list.arrays_equal({  }, { "a" }))
+    luaunit.assertFalse(list.arrays_equal({ "a" }, { }))
+end
+
 function test_string_list()
     local tests = {
         "asdf",
